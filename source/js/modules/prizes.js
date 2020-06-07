feb0580e72ac8e257915e8377d2b5e4e95940de7
Anastasiya <v.anastasiya26@mail.ru>
2020-06-07 18:50:06 +0700

    задание 10

--- source/js/modules/prizes.js
+++ source/js/modules/prizes.js
@@ -1,7 +1,12 @@
 export default () => {
   const primaryPrize = document.querySelector(`.prizes__item--journeys`);
   const primaryPrizeImage = primaryPrize.querySelector(`.prizes__icon img`);
+
+  const secondaryPrize = document.querySelector(`.prizes__item--cases`);
+  const secondaryPrizeImage = secondaryPrize.querySelector(`.prizes__icon img`);
+
   primaryPrize.addEventListener(`animationstart`, () => {
     primaryPrizeImage.src = primaryPrizeImage.dataset.src;
+    secondaryPrizeImage.src = `img/secondary-award.svg`;
   });
 };
