70cc233d5700023452a48ef6329c3f2ee349f507
vasinskaya.aa <v.anastasiya26@mail.ru>
2020-10-06 14:15:50 +0700

    draw back

--- source/js/modules/result.js
+++ source/js/modules/result.js
@@ -46,4 +46,37 @@ export default () => {
       });
     }
   }
+
+  const sizesCanvas = {
+    back: {
+      width: 586,
+      height: 324
+    }
+  };
+
+  const backCanvas = document.querySelector(`canvas#back`);
+  if (backCanvas.getContext) {
+    const backContext = backCanvas.getContext(`2d`);
+
+    const drawBackCanvas = () => {
+      let windowWidth = window.innerWidth;
+      let windowHeight = window.innerHeight;
+      backCanvas.width = windowWidth;
+      backCanvas.height = windowHeight;
+      backContext.globalAlpha = 1;
+      backContext.clearRect(0, 0, windowWidth, windowHeight);
+      backContext.save();
+      backContext.drawImage(backImg, Math.round((windowWidth - sizesCanvas.back.width) / 2), Math.round((windowHeight - sizesCanvas.back.height) / 2), sizesCanvas.back.width, sizesCanvas.back.height);
+      backContext.restore();
+    };
+
+    const backImg = new Image();
+    backImg.onload = () => {
+      drawBackCanvas();
+    };
+
+    backImg.src = `/img/back.png`;
+    window.addEventListener(`resize`, drawBackCanvas);
+  }
+
 };
